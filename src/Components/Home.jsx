90fdbb77232a6cf5c709eadd32836cc0b7766676
Saisundar-AV <saisundarvelmurugan@gmail.com>
2025-02-25 08:20:01 +0530

    empty

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,7 +2,7 @@ import '../CSS/Home.css';
 function Home(){
     return (
         <main>
-            <img src="public/Candidate_Photo.jpg" height="347px" width="247px" ></img>
+            <img src="public\Candidate_Photo.jpg" height="347px" width="247px" ></img>
             <h1 className='desi'>I am Saisundar</h1>
             <h1></h1>
         </main>
